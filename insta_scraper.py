b87f1e448121cb4d4d844e636b4c4124a03c256e
theod07 <abc@def.com>
2016-03-05 22:50:01 -0800

    create script to scrape using selenium

--- insta_scraper.py
+++ insta_scraper.py
@@ -1,10 +1,11 @@
-import requests
-import time
-import json
+from bs4 import BeautifulSoup
+import selenium.webdriver as webdriver
 
-with open('/Users/wonder/.insta.json', 'r') as f:
-    TOKEN = json.load(f)['access_token']
+url = 'http://instagram.com/'
+driver = webdriver.Firefox()
+driver.get(url)
 
-url = 'https://api.instagram.com/v1/users/self/?access_token='
-# url = 'https://api.instagram.com/v1/users/500934533/?access_token='
-data = requests.get(url+TOKEN)
+soup = BeautifulSoup(driver.page_source)
+
+for x in soup.findAll('li', {'class':'photo'}):
+    print x
