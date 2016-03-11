588662d04a2f3bca18f8c3970637526ef4c09e8f
theod07 <abc@def.com>
2016-03-10 23:42:55 -0800

    start building script to implement vgg16 nnet

---
+++ code/vgg16_model/model.py
@@ -0,0 +1,55 @@
+import numpy as np
+import lasagne
+from lasagne.layers import InputLayer
+from lasagne.layers import DenseLayer
+from lasagne.layers import NonlinearityLayer
+from lasagne.layers import DropoutLayer
+from lasagne.layers import Pool2DLayer as PoolLayer
+from lasagne.layers.dnn import Conv2DDNNLayer as ConvLayer
+from lasagne.nonlinearities import softmax
+import pickle
+import vgg16
+import urllib
+import io
+import skimage.transform
+
+model = pickle.load(open('vgg16.pkl'))
+CLASSES = model['synset words']
+MEAN_IMAGE = model['mean value']
+
+nnet = vgg16.build_model()
+
+lasagne.layers.set_all_param_values(output_layer, model['param values'])
+
+
+index = urllib.urlopen('http://www.image-net.org/challenges/LSVRC/2012/ori_urls/indexval.html').read()
+image_urls = index.split('<br>')
+
+np.random.seed(23)
+np.random.shuffle(image_urls)
+image_urls = image_urls[:5]
+
+def prep_image(url):
+    ext = url.split('.')[-1]
+    im = plt.imread(io.BytesIO(urllib.urlopen(url).read()), ext)
+    # Resize so smallest dim = 256, preserving aspect ratio
+    h, w, _ = im.shape
+    if h < w:
+        im = skimage.transform.resize(im, (256, w*256/h), preserve_range=True)
+    else:
+        im = skimage.transform.resize(im, (h*256/w, 256), preserve_range=True)
+
+    # Central crop to 224x224
+    h, w, _ = im.shape
+    im = im[h//2-112:h//2+112, w//2-112:w//2+112]
+
+    rawim = np.copy(im).astype('uint8')
+    
+    # Shuffle axes to c01
+    im = np.swapaxes(np.swapaxes(im, 1, 2), 0, 1)
+
+    # Convert to BGR
+    im = im[::-1, :, :]
+
+    im = im - MEAN_IMAGE
+    return rawim, floatX(im[np.newaxis])
