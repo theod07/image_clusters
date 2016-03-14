0115a49c71cd7564449c1ab979376c2615ef9e9b
theod07 <abc@def.com>
2016-03-14 12:47:48 -0700

    debug

--- code/pipeline.py
+++ code/pipeline.py
@@ -1,8 +1,9 @@
 import download_imgs as dl
-from download_imgs import has_src_url_file
-from download_imgs import read_src_urls
-from database import vec_to_str
-from database import insert_prediction
+# from download_imgs import has_src_url_file
+# from download_imgs import read_src_urls
+import database as db
+# from database import vec_to_str
+# from database import insert_prediction
 from vgg16_model import model as nn
 
 
@@ -17,13 +18,13 @@ if __name__ == '__main__':
 
     username = raw_input('give me a username: ')
 
-    while has_src_url_file(username):
-        src_urls = read_src_urls(username)
+    while dl.has_src_url_file(username):
+        src_urls = dl.read_src_urls(username)
 
         for url in src_urls:
-            s3_save(username, url, bucket)
+            dl.s3_save(username, url, bucket)
             pred = nn.predict(url)[0]
-            pred_str = vec_to_str(pred)
-            insert_prediction(url, pred_str)
+            pred_str = db.vec_to_str(pred)
+            db.insert_prediction(url, pred_str)
 
     conn.close()
