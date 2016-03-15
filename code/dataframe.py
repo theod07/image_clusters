f022c25961d2f6c72cb2a0c39afe95172abd3833
theod07 <abc@def.com>
2016-03-15 15:46:43 -0700

    create file dataframe.py to save predictions to dataframe & pickle DF for each user

--- code/dataframe.py
+++ code/dataframe.py
@@ -3,38 +3,33 @@ import numpy as np
 import cPickle
 import download_imgs as dl
 import random
-import time
+# from vgg16_model import model as nn
+
+
 def rand_samp(items, k=100):
     if len(items) <= k:
         return items
     else:
         return random.sample(items, k)
 
-username = 'year'
-src_urls = dl.get_src_urls(username)
-src_urls_samp = rand_samp(src_urls)
+if __name__ == '__main__':
 
-deltas = []
-for i in xrange(5000):
-    tic = time.clock()
-    preds = []
-    for i in xrange(1000):
-        #simulate a prediction
-        prediction = np.random.rand(1000)
-        preds.append(prediction)
-    toc = time.clock()
-    # print 'delta: {}'.format(toc-tic)
-    deltas.append(toc-tic)
-print 'mean delta (list): {}'.format(np.mean(deltas))
+    usernames = ['year', 'oceana', 'paolatonight', 'patricknorton']
 
-deltas = []
-for i in xrange(5000):
-    tic = time.clock()
-    preds = np.zeros([1000,1000])
-    for i in xrange(1000):
-        #simulate a prediction
-        prediction = np.random.rand(1000)
-        preds[i,:] = prediction
-    toc = time.clock()
-    deltas.append(toc-tic)
-print 'mean delta (numpy): {}'.format(np.mean(deltas))
+    for username in usernames:
+        src_urls = dl.get_src_urls(username)
+        src_urls_samp = rand_samp(src_urls)
+        preds = []
+
+        for url in src_urls_samp:
+            #simulate a prediction
+            pred = np.random.rand(1000)
+            # pred = nn.predict(url)[0]
+            preds.append(pred)
+
+        df = pd.DataFrame(zip(src_urls_samp, preds), columns=['src_url','prediction'])
+        fname = '../data/{}/{}_predictions.pkl'.format(username, username)
+        df.to_pickle(fname)
+        with open('../logs/log_dataframe.txt', 'ab') as f:
+            f.write('dataframe saved to {}'.format(fname))
+        print 'dataframe saved to {}'.format(fname)
