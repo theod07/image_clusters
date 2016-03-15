ae374e63b258a15ed5d1706359f4fe4412c3d597
theod07 <abc@def.com>
2016-03-15 16:17:51 -0700

    add timestamp to log

--- code/dataframe.py
+++ code/dataframe.py
@@ -4,6 +4,7 @@ import cPickle
 import download_imgs as dl
 import random
 import os
+from time import strftime
 # from vgg16_model import model as nn
 
 def rand_samp(items, k=100):
@@ -14,8 +15,6 @@ def rand_samp(items, k=100):
 
 def has_predictions_pkl(user):
     fname = '{}_predictions.pkl'.format(user)
-    print 'fname: {}'.format(fname)
-    print 'os.listdir: {}'.format(os.listdir('../data/{}/'.format(user)))
     if fname in os.listdir('../data/{}/'.format(user)):
         return True
     return False
@@ -39,5 +38,5 @@ if __name__ == '__main__':
         fname = '../data/{}/{}_predictions.pkl'.format(user, user)
         df.to_pickle(fname)
         with open('../logs/log_dataframe.txt', 'ab') as f:
-            f.write('dataframe saved to {}'.format(fname))
-        print 'dataframe saved to {}'.format(fname)
+            f.write('{} dataframe saved to {}'.format(strftime('%Y%m%d.%H:%M:%s'), fname))
+        print '{} dataframe saved to {}'.format(strftime('%Y%m%d.%H:%M:%s'), fname)
