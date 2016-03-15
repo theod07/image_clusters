7db3f9d7f932aca3e25302238036cdff0ccc7776
theod07 <abc@def.com>
2016-03-14 23:15:17 -0700

    remove raw_input() from __main__

--- code/pipeline.py
+++ code/pipeline.py
@@ -20,18 +20,19 @@ if __name__ == '__main__':
     pg_conn = pg2.connect(user='postgres', password='admin', dbname='image_clusters')
     pg_cursor = pg_conn.cursor()
 
-    username = raw_input('give me a username: ')
-    # year, oceana, paolatonight, patricknorton
+    # username = raw_input('give me a username: ')
+    usernames = [year, oceana, paolatonight, patricknorton]
 
-    while dl.has_src_url_file(username):
-        shortcodes = dl.get_shortcodes(username)
-        src_urls = dl.get_src_urls(username)
+    for user in usernames:
+        while dl.has_src_url_file(username):
+            shortcodes = dl.get_shortcodes(username)
+            src_urls = dl.get_src_urls(username)
 
 
-        for (code,url) in zip(shortcodes, src_urls):
-            dl.s3_save(username, url, bucket)
-            pred = nn.predict(url)[0]
-            pred_str = db.vec_to_str(pred)
-            db.insert_prediction(username, code, url, pred_str, pg_conn)
+            for (code,url) in zip(shortcodes, src_urls):
+                dl.s3_save(username, url, bucket)
+                pred = nn.predict(url)[0]
+                pred_str = db.vec_to_str(pred)
+                db.insert_prediction(username, code, url, pred_str, pg_conn)
 
     s3_conn.close()
