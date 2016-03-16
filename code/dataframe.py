30a77e45c5c7e8193915d3a8df26ffeca8e00e5e
theod07 <abc@def.com>
2016-03-15 17:15:18 -0700

    edit datframe.py

--- code/dataframe.py
+++ code/dataframe.py
@@ -15,7 +15,7 @@ def rand_samp(items, k=100):
 
 def has_predictions_pkl(user):
     fname = '{}_predictions.pkl'.format(user)
-    if fname in os.listdir('../data/{}/'.format(user)):
+    if fname in os.listdir('../pickles/'.format(user)):
         return True
     return False
 
@@ -37,8 +37,8 @@ if __name__ == '__main__':
             preds.append(pred)
 
         df = pd.DataFrame(zip(src_urls_samp, preds), columns=['src_url','prediction'])
-        fname = '../data/{}/{}_predictions.pkl'.format(user, user)
+        fname = '../pickles/{}_predictions.pkl'.format(user)
         df.to_pickle(fname)
         with open('../logs/log_dataframe.txt', 'ab') as f:
-            f.write('{} dataframe saved to {}'.format(strftime('%Y%m%d.%H:%M:%s'), fname))
-        print '{} dataframe saved to {}'.format(strftime('%Y%m%d.%H:%M:%s'), fname)
+            f.write('{} dataframe saved to {}\n'.format(strftime('%Y%m%d.%H:%M:%s'), fname))
+        print '{} dataframe saved to {}\n'.format(strftime('%Y%m%d.%H:%M:%s'), fname)
