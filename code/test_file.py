c0b77890b3b993f07832d3531d582069ce6f492f
lasagne_instance <theo@aws_lasagne_instance>
2016-03-23 23:23:37 +0000

    troubleshooting

--- code/test_file.py
+++ code/test_file.py
@@ -1,5 +1,5 @@
 import os
-# from vgg16_model import vgg16
+from vgg16_model import vgg16
 from vgg16_model import vgg16_cpu
 from vgg16_model import model
 import cPickle as pickle
@@ -9,8 +9,8 @@ model = pickle.load(open('./vgg16_model/vgg16.pkl'))
 CLASSES = model['synset words']
 MEAN_IMAGE = model['mean value'][:, np.newaxis, np.newaxis] # necessary to match dimensions of incoming images
 
-# nnet = vgg16.build_model()
-nnet = vgg16_cpu.build_model()
+nnet = vgg16.build_model()
+# nnet = vgg16_cpu.build_model()
 
 lasagne.layers.set_all_param_values(nnet['prob'], model['param values'])
 
